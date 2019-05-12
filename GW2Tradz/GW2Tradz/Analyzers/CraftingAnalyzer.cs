43f1c5ce3f303b06648f3b903655522735b03035
krismaz <krislinux@gmail.com>
2019-05-12 21:09:54 +0200

    Filter crafting analyzer

--- GW2Tradz/GW2Tradz/Analyzers/CraftingAnalyzer.cs
+++ GW2Tradz/GW2Tradz/Analyzers/CraftingAnalyzer.cs
@@ -13,6 +13,8 @@ namespace GW2Tradz.Analyzers
     {
         public List<TradingAction> Analyse(Cache cache)
         {
+            var dailyRecipes = new HashSet<int> {9790, 9792 };
+
             var items = cache.Lookup;
             var validRecipes = cache.Recipes
                     .Where(r => r.Ingredients.All(i => i.ItemId == -1 || items.ContainsKey(i.ItemId)) & items.ContainsKey(r.OutputItemId));
@@ -21,11 +23,19 @@ namespace GW2Tradz.Analyzers
 
             foreach (var recipe in validRecipes)
             {
+
                 var item = items[recipe.OutputItemId];
+
+                if (recipe.Disciplines.Contains("Mystic Forge") && (item.Name.Contains("Rune") || item.Name.Contains("Sigil")))
+                {
+                    continue;
+                }
+
+
                 var cost = recipe.Ingredients.Sum(i => i.Count * (i.ItemId == -1 ? 1 : items[i.ItemId].SellPrice))/recipe.OutputItemCount;
                 result.Add(new TradingAction
                 {
-                    MaxAmount = (int)item.AdjustedSellVelocity - Settings.VelocityUncertainty - cache.CurrentSells[recipe.OutputItemId],
+                    MaxAmount = dailyRecipes.Contains(recipe.Id) ? 1 : (int)item.AdjustedSellVelocity - Settings.VelocityUncertainty - cache.CurrentSells[recipe.OutputItemId],
                     Description = $"{item.Name} - {string.Join(", ", recipe.Disciplines)} ({string.Join(", ", recipe.Ingredients.Select(i => i.ItemId == -1 ? "Coin" : items[i.ItemId].Name))})",
                     Item = item,
                     CostPer = (int)cost,
