16f6a26ec8ec58c098deeb730c1c23efc99d99f5
krismaz <krislinux@gmail.com>
2019-05-12 19:35:49 +0200

    Update the UI

--- GW2Tradz/GW2Tradz/MainWindow.xaml.cs
+++ GW2Tradz/GW2Tradz/MainWindow.xaml.cs
@@ -3,6 +3,7 @@ using GW2Tradz.Networking;
 using GW2Tradz.Viewmodels;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -30,22 +31,20 @@ namespace GW2Tradz
             var cache = new Cache();
             cache.Load();
 
-            if(Settings.TotalGold == -1)
+            if (Settings.TotalGold == -1)
             {
                 Settings.TotalGold = cache.WalletGold + cache.CurrentBuys.Values.Sum() + cache.DeliveryBox.Coins;
             }
 
-            MainGrid.ItemsSource = cache.Items;
             var flipping = new FlippingAnalyzer();
             var dyes = new DyeSalvagingAnalyzer();
             var crafting = new CraftingAnalyzer();
             var ecto = new EctoAnalyzer();
-            FlippingGrid.ItemsSource = flipping.Analyse(cache);
-            DyeGrid.ItemsSource = dyes.Analyse(cache);
-            CraftingGrid.ItemsSource = crafting.Analyse(cache);
-            EctoGrid.ItemsSource = ecto.Analyse(cache);
-            CombinedGrid.ItemsSource = new CombiningAnalyzer { Analyzers = new List<IAnalyzer> { flipping, dyes, crafting, ecto } }.Analyse(cache);
-
+            FlippingGrid.DataContext = flipping.Analyse(cache);
+            DyeGrid.DataContext = dyes.Analyse(cache);
+            CraftingGrid.DataContext = crafting.Analyse(cache);
+            EctoGrid.DataContext = ecto.Analyse(cache);
+            CombinedGrid.DataContext = new CombiningAnalyzer { Analyzers = new List<IAnalyzer> { flipping, dyes, crafting, ecto } }.Analyse(cache);
         }
 
         private void MainGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
@@ -56,6 +55,25 @@ namespace GW2Tradz
         private void FlippingGrid_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
             Clipboard.SetText(e.AddedItems.OfType<TradingAction>().FirstOrDefault()?.Item?.Name ?? "");
+        }
+
+        private void DataGrid_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is DataGrid grid && grid.SelectedValue is TradingAction action)
+            {
+                Process.Start("https://www.gw2bltc.com/en/item/" + action.Item.Id);
+            }
+        }
+
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            if (sender is DataGrid grid)
+            {
+                var view = CollectionViewSource.GetDefaultView(grid.ItemsSource);
+                view.SortDescriptions.Clear();
+                view.SortDescriptions.Add(new System.ComponentModel.SortDescription { PropertyName = "Profit", Direction = System.ComponentModel.ListSortDirection.Descending });
+                view.Refresh();
+            }
         }
     }
 }
