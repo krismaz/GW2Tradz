77fb6a65fa1ece0440df97babafc42ad485f08f6
krismaz <krislinux@gmail.com>
2019-06-30 20:56:39 +0200

    Much better gemstones

--- GW2Tradz/GW2Tradz/Analyzers/GemstoneAnalyzer.cs
+++ GW2Tradz/GW2Tradz/Analyzers/GemstoneAnalyzer.cs
@@ -35,6 +35,24 @@ namespace GW2Tradz.Analyzers
                     SafeProfitPercentage = Settings.SafeMinimumMargin
                 });
             }
+            var gemstoneIds = gemstones.Select(i => i.Id).ToList();
+            foreach (var recipe in cache.Recipes.Where(r=>r.Id>0 && gemstoneIds.Contains(r.OutputItemId)))
+            {
+                var crystal = recipe.Ingredients.Select(i=> cache.Lookup[i.ItemId]).Where(i => !i.Name.Contains("Dust")).FirstOrDefault(); //What's the dust called again?
+                var totalCost = 5 * ecto.FlipBuy + 75 * recipe.Ingredients.Sum(i=>i.Count * cache.Lookup[i.ItemId].FlipBuy);
+                var totalIncome = 11.5 * amal.FlipSell;
+
+                result.Add(new TradingAction
+                {
+                    MaxAmount = (int)crystal.AdjustedBuyVelocity,
+                    Description = $"Craft from {crystal.Name} and mystic forge to Gemstone",
+                    Item = crystal,
+                    CostPer = totalCost / 75,
+                    IncomePer = (int)(totalIncome / 75).AfterTP(),
+                    BaseCost = Settings.EasyTaskCost,
+                    SafeProfitPercentage = Settings.SafeMinimumMargin
+                });
+            }
 
             return result;
         }
