e03b56c5a8e3004fd1ad86a69d5ab89e21c73d2d
krismaz <krislinux@gmail.com>
2019-08-22 16:14:58 +0200

    Blame silvers server when it is down

--- GW2Tradz/GW2Tradz/Networking/silveress.cs
+++ GW2Tradz/GW2Tradz/Networking/silveress.cs
@@ -10,6 +10,7 @@ using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace GW2Tradz.Networking
 {
@@ -32,22 +33,38 @@ namespace GW2Tradz.Networking
 
         public List<Item> FetchBasicInfo()
         {
-            var result = httpClient.GetAsync("https://api.silveress.ie/gw2/v1/items/json?beautify=min&fields=id,buy_price,sell_price,name,type,rarity,vendor_value,7d_sell_sold,7d_buy_sold,12m_sell_price_avg,1m_sell_price_avg,level,statName,upgrade1").Result; //worst coding practice or worsest coding practice
-            var content = result.Content.ReadAsStringAsync().Result;
-            return JsonConvert.DeserializeObject<List<Item>>(content, jsonSettings);
+            try
+            {
+                var result = httpClient.GetAsync("https://api.silveress.ie/gw2/v1/items/json?beautify=min&fields=id,buy_price,sell_price,name,type,rarity,vendor_value,7d_sell_sold,7d_buy_sold,12m_sell_price_avg,1m_sell_price_avg,level,statName,upgrade1").Result; //worst coding practice or worsest coding practice
+                var content = result.Content.ReadAsStringAsync().Result;
+                return JsonConvert.DeserializeObject<List<Item>>(content, jsonSettings);
+            }
+            catch
+            {
+                MessageBox.Show("Silveress comms err");
+                throw;
+            }
         }
 
         public List<History> FetchHistory(IEnumerable<int> ids)
         {
-            var OneWeek = (DateTime.Now - TimeSpan.FromDays(7)).ToString("yyyy-MM-dd");
-            var buffer = new List<History> { };
-            foreach (var chunk in ids.Chunk(100))
+            try
             {
-                var result = httpClient.GetAsync($"https://api.silveress.ie/gw2/v1/history?beautify=min&start={OneWeek}&fields=itemID,date,sell_sold,buy_sold,sell_price_max&filter=type:day&itemID=" + string.Join(", ", chunk)).Result; //worst coding practice or worsest coding practice
-                var content = result.Content.ReadAsStringAsync().Result;
-                buffer.AddRange(JsonConvert.DeserializeObject<List<History>>(content, jsonSettings).ToList());
+                var OneWeek = (DateTime.Now - TimeSpan.FromDays(7)).ToString("yyyy-MM-dd");
+                var buffer = new List<History> { };
+                foreach (var chunk in ids.Chunk(100))
+                {
+                    var result = httpClient.GetAsync($"https://api.silveress.ie/gw2/v1/history?beautify=min&start={OneWeek}&fields=itemID,date,sell_sold,buy_sold,sell_price_max&filter=type:day&itemID=" + string.Join(", ", chunk)).Result; //worst coding practice or worsest coding practice
+                    var content = result.Content.ReadAsStringAsync().Result;
+                    buffer.AddRange(JsonConvert.DeserializeObject<List<History>>(content, jsonSettings).ToList());
+                }
+                return buffer;
             }
-            return buffer;
+            catch
+            {
+                MessageBox.Show("Silveress history comms err");
+                return new List<History> { };
+            }
         }
     }
 }
